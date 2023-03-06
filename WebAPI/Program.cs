d395a400c0f48811d4e73b37013a542ffa4080b2
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2023-03-06 14:43:50 +0300

    Fluent Validation Middleware

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Application.Validators.Posts;
+using FluentValidation.AspNetCore;
+using Infrastructure.Filters;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +12,9 @@ builder.Services.AddCors(options=>options.AddDefaultPolicy(policy =>
     policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()
 ));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(congifuration =>congifuration.RegisterValidatorsFromAssemblyContaining<CreatePostValidator>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
