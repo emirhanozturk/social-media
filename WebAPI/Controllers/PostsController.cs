19349795806b748a0d0481da530ebe4ccbf6cad4
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2023-03-08 18:40:05 +0300

    File Upload Work

--- WebAPI/Controllers/PostsController.cs
+++ WebAPI/Controllers/PostsController.cs
@@ -1,7 +1,8 @@
 ﻿using Application.Repositories;
-using Domain.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Application.Dtos;
+using System.Net;
+using Application.RequestParamaters;
 
 namespace WebAPI.Controllers
 {
@@ -10,10 +11,35 @@ namespace WebAPI.Controllers
     public class PostsController : ControllerBase
     {
 
-        [HttpGet]
-        public async Task Get()
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PostsController(IWebHostEnvironment webHostEnvironment)
         {
-            
+
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            //wwwroot/resource/post-images
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath,"resource/post-images");
+
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            Random r = new();
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None,1024*1024,useAsync:false);
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            return Ok();
         }
 
         
